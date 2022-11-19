0a7ac588a225ade61fabe4ae3ea78f169004ac4b
Bernard <beniek12@onet.pl>
2022-11-19 14:42:54 +0100

    Done html markup and styled it in css

--- app.js
+++ app.js
@@ -11,38 +11,30 @@ function game() {
   }
 
   function getPlayerChoice() {
-    let choice = prompt("Rock, paper or scissors?").toLowerCase();
-    if (choice === moves[0] || choice === moves[1] || choice === moves[2]) {
-      console.log(choice);
-    } else {
-      choice = prompt("Rock, paper or scissors?").toLowerCase();
-    }
-    return choice;
-  }
+    let cpuMove = getComputerChoice();
+    let playerMove = getPlayerChoice();
 
-  let cpuMove = getComputerChoice();
-  let playerMove = getPlayerChoice();
-
-  if (cpuMove === playerMove) {
-    window.alert("Draw");
-  } else if (cpuMove === "rock") {
-    if (playerMove === "paper") {
-      window.alert("Player wins");
+    if (cpuMove === playerMove) {
+      window.alert("Draw");
+    } else if (cpuMove === "rock") {
+      if (playerMove === "paper") {
+        window.alert("Player wins");
+      } else {
+        window.alert(`${cpuMove} Player loses`);
+      }
+    } else if (cpuMove === "paper") {
+      if (playerMove === "scissors") {
+        window.alert("Player wins");
+      } else {
+        window.alert(`${cpuMove} wins`);
+      }
     } else {
-      window.alert(`${cpuMove} Player loses`);
+      if (playerMove === "rock") {
+        window.alert("Player wins");
+      } else {
+        window.alert(`${cpuMove} wins`);
+      }
     }
-  } else if (cpuMove === "paper") {
-    if (playerMove === "scissors") {
-      window.alert("Player wins");
-    } else {
-      window.alert(`${cpuMove} wins`);
-    }
-  } else {
-    if (playerMove === "rock") {
-      window.alert("Player wins");
-    } else {
-      window.alert(`${cpuMove} wins`);
-    }
+    console.log(cpuMove + playerMove);
   }
-  console.log(cpuMove + playerMove);
 }
